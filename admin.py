d8d0257410bb0bf7981e3cea4f49c10eb5e835ea
nabinpandey <nabinpandey967@gmail.com>
2025-11-15 21:41:09 +0545

    added list book feature

--- admin.py
+++ admin.py
@@ -10,6 +10,9 @@ class admin_menu():
             print(f"{self.id.pop(index)} {self.title[index]} removed successfully.")
         else:
             print("Book ID not found.")
+    def list_books(self):
+        for i in range(len(self.id)):
+            print(f"Book ID: {self.id[i]}, Title: {self.title[i]}")
 
         
        
