eff030a3948f34d4c7ba4c48a6c9e62b46e0eff2
nabinpandey <nabinpandey967@gmail.com>
2025-11-16 12:20:16 +0545

    added search and borrow in member.py

--- b_member.py
+++ b_member.py
@@ -1,2 +1,24 @@
 import admin as amn
-
+class member_menu():
+    def __init__(self):
+        self.admin = amn.admin_menu()
+    def search_book(self, book_title="none", book_id=0):
+        if book_id in self.admin.id or book_title in self.admin.title:
+            print(f"Book Found: {book_id} - {book_title}")
+        else:
+            print("Book not found.")
+    def book_borrow(self, book_id,member_name, member_id):
+        index = self.admin.id.index[book_id]
+        if book_id in self.admin.id:
+            print(f"""Book-details 
+                   ID: {book_id} 
+                   Name : {self.admin.title[index]}
+                   Borrowed by 
+                   Name : {member_name} 
+                   ID: {member_id}""")
+            self.admin.id.remove(book_id)
+            self.admin.title.remove(self.admin.title[index])
+        else:
+            print("Book ID not found, cannot borrow.")
+    
+    
